9298328a8747a5796becea9a0b0a5485721aeb6c
CoderEslam <eslamghazy600@gmail.com>
2023-02-16 22:55:16 +0200

    Initial commit

---
+++ app/src/main/java/com/doubleclick/switchbutton/MainActivity.java
@@ -0,0 +1,32 @@
+package com.doubleclick.switchbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SwitchButton switchButton =  findViewById(R.id.switch_button);
+
+        switchButton.setChecked(true);
+        switchButton.isChecked();
+        switchButton.toggle();     //switch state
+        switchButton.toggle(false);//switch without animation
+        switchButton.setShadowEffect(true);//disable shadow effect
+        switchButton.setEnabled(false);//disable button
+        switchButton.setEnableEffect(false);//disable the switch animation
+        switchButton.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                //TODO do your job
+            }
+        });
+
+    }
+}
